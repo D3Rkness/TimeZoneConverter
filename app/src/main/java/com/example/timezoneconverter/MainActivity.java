c90c9bf443b6e3440f0a10d7d8009a9dbeb2fc69
Jonathan He <d3rk12345678@gmail.com>
2024-02-15 00:44:02 -0500

    Time conversion implementation

--- app/src/main/java/com/example/timezoneconverter/MainActivity.java
+++ app/src/main/java/com/example/timezoneconverter/MainActivity.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.util.Log;
+
 
 import java.util.Locale;
 
@@ -14,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
     Button timeButton;
     int hour, minute;
     String period;
+    TextView homeTimeZoneText;
+    Spinner currentTimeZoneText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timeButton = findViewById(R.id.timeButton);
+        homeTimeZoneText = findViewById(R.id.homeTimeText);
+        currentTimeZoneText = (Spinner)findViewById(R.id.timeZoneSpinner);
     }
 
     public void popTimePicker(View view) {
@@ -46,6 +55,47 @@ public class MainActivity extends AppCompatActivity {
 
         timePickerDialog.setTitle("Select Time");
         timePickerDialog.show();
+    }
+
+    public void convertTime(View view) {
+        // Obtain information about the home time zone
+        String homeTimeZone = homeTimeZoneText.getText().toString();
+        char homeTimeSign = homeTimeZone.charAt(homeTimeZone.length() - 6);
+        int homeTimeDigit = (homeTimeZone.charAt(homeTimeZone.length() - 4)) - '0';
+
+        // Obtain information about the current time zone
+        String currTimeZone = currentTimeZoneText.getSelectedItem().toString();
+        char currTimeSign = currTimeZone.charAt(currTimeZone.length() - 6);
+        int currTimeDigit = (currTimeZone.charAt(currTimeZone.length() - 4)) - '0';
+
+        int convertedHour = hour;
+
+        if (homeTimeSign == currTimeSign) {
+            convertedHour += currTimeDigit - homeTimeDigit;
+        } else if (homeTimeSign == '+') {
+            convertedHour += -1 * (homeTimeDigit + currTimeDigit);
+        } else {
+            convertedHour += homeTimeDigit + currTimeDigit;
+        }
+
+        if (convertedHour < 1) {
+            convertedHour += 12;
+            if (period.equals("AM")) {
+                period = "PM";
+            } else {
+                period = "AM";
+            }
+        } else if (convertedHour > 12) {
+            convertedHour -= 12;
+            if (period.equals("AM")) {
+                period = "PM";
+            } else {
+                period = "AM";
+            }
+        }
+
+        TextView convertedTimeText = findViewById(R.id.convertedTimeText);
+        convertedTimeText.setText(String.format(Locale.getDefault(), "%02d:%02d %s", convertedHour, minute, period));
     }
 
     public void viewSettings(View view) {
