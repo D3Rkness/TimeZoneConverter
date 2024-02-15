c489ed57d9c5b14a86a18c9dbf688e314a46c732
Jonathan He <d3rk12345678@gmail.com>
2024-02-14 23:54:37 -0500

    Creation and navigation of save button in settings

--- app/src/main/java/com/example/timezoneconverter/SettingsActivity.java
+++ app/src/main/java/com/example/timezoneconverter/SettingsActivity.java
@@ -3,7 +3,9 @@ package com.example.timezoneconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
+    }
+
+    public void returnToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
