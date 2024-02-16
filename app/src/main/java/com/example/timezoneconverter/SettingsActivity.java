62a3364d4b4b324aaa10786e3b62b046af619e06
Jonathan He <d3rk12345678@gmail.com>
2024-02-16 13:27:17 -0500

    Implement saved preferences for home time zone spinner

--- app/src/main/java/com/example/timezoneconverter/SettingsActivity.java
+++ app/src/main/java/com/example/timezoneconverter/SettingsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class SettingsActivity extends AppCompatActivity {
     TextInputEditText homeCityEdit;
     SharedPreferences myPrefs;
+    Spinner settingsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +22,60 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
 
-        // Find home city text component of page
+        // Find all components of page
         homeCityEdit = findViewById(R.id.editCityText);
+        settingsSpinner = findViewById(R.id.settingsTimeZoneSpinner);
 
         // Create shared preferences
         Context context = getApplicationContext();
         myPrefs = context.getSharedPreferences("AppSettings", Context.MODE_PRIVATE);
 
-        // Set home city text
+        // Set home time zone and city text
         homeCityEdit.setText(myPrefs.getString("HomeCityName", "Baltimore"));
+        settingsSpinner.setSelection(myPrefs.getInt("TimeZoneSelectionNumber", 0));
     }
 
     public void returnToMain(View view) {
         // Obtain the text from spinner and home city
-        Spinner settingsSpinner = findViewById(R.id.settingsTimeZoneSpinner);
         String selectedTimeZone = settingsSpinner.getSelectedItem().toString();
         String cityName = "";
         if (homeCityEdit.getText() != null) {
             cityName = homeCityEdit.getText().toString();
         }
 
+        // Find corresponding selection number in spinner for the selected time zone
+        int selectionNumber;
+        switch (selectedTimeZone) {
+            case ("America/Los_Angeles: GMT -08:00"):
+                selectionNumber = 1;
+                break;
+            case ("Europe/Berlin: GMT +01:00"):
+                selectionNumber = 2;
+                break;
+            case ("Europe/Istanbul: GMT +02:00"):
+                selectionNumber = 3;
+                break;
+            case ("Asia/Bangkok: GMT +07:00"):
+                selectionNumber = 4;
+                break;
+            case ("Asia/Singapore: GMT +08:00"):
+                selectionNumber = 5;
+                break;
+            case ("Asia/Tokyo: GMT +09:00"):
+                selectionNumber = 6;
+                break;
+            case ("Australia/Canberra: GMT +10:00"):
+                selectionNumber = 7;
+                break;
+            default:
+                selectionNumber = 0;
+                break;
+        }
+
         // Set shared preferences
         SharedPreferences.Editor editor = myPrefs.edit();
         editor.putString("SelectedTimeZone", selectedTimeZone);
+        editor.putInt("TimeZoneSelectionNumber", selectionNumber);
         editor.putString("HomeCityName", cityName);
         editor.apply();
 
